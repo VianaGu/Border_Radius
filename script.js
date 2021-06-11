d13fd55d627dca218555e06d7c049890dc0e20dd
gustavolive0603 <gustaolive1234@gmail.com>
2021-06-11 11:51:31 -0300

    Altera a maneira de atualizar a div "mid"
    No commit anterior a div "mid" era atualizada por um evento de escuta
    implementado ao botão no rodapé da página
    
    A alteração foi para que todas vez que o usuarios insere um valor a
    página atualiza automaticamente

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const text = document.getElementById("text");
 const button = document.getElementById("button");
 
-button.addEventListener('click',setRadius);
+text.addEventListener('keyup',setRadius);
+button.addEventListener("click", myFunction)
+
 /* Funções */
-function setRadius(value){
+function setRadius(){
     /* Variaveis locais */
     const valor = document.getElementById("text").value;
-
     document.getElementById("mid").style.borderRadius = valor + "px";
-   
 }
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("text");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("border-radius: " + copyText.value+"px;");
+  }
