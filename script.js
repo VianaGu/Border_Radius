a06ffe6d90cf3bc6330f04ff9ae8e2c64adc6750
gustavolive0603 <gustaolive1234@gmail.com>
2021-06-12 20:56:42 -0300

    Melhora geração de código
    de acordo com as opcões pelo usuario escolhidas o codigo pe criado de
    uma maneira diferente

--- script.js
+++ script.js
@@ -1,65 +1,119 @@
 /* variaveis Globais */
 const text = document.getElementById("text");
 const button = document.getElementById("button");
-const v1=0;
 const tela = document.getElementById("mid");
 const opt = document.getElementById("test");
 const SE = document.getElementById("SuperiorEsquerdo");
 const SD = document.getElementById("SuperiorDireito");
 const IE = document.getElementById("InferiorEsquerdo");
 const ID = document.getElementById("InferiorDireito");
+var num = new Array(
+    '',
+    '',
+    '',
+    '',
+    ''
+);
 
 text.focus();
 
 /* Eventos */
 text.addEventListener('keyup',setRadius);
-button.addEventListener("click", myFunction);
-opt.addEventListener("click",zero);
-window.addEventListener("click", checkedBox);
+button.addEventListener("click", myButton);
+window.addEventListener("click",checkedBox);
+opt.addEventListener("click",checkedBox1);
 window.addEventListener("click", setRadius);
+
+
 /* Funções */
-function zero(){
+function getValor(qnum){
+    var qnum;
+    return num[qnum]
+}
+function guardaValor(retorno){
+
+    
+    var retorno;
+    if(opt.checked == true){
+        num[0] = retorno;
+    }if(SE.checked == true){
+        num[1] = retorno;       
+    }if(SD.checked == true){
+        num[2] = retorno;
+    }if(IE.checked == true){
+        num[3] = retorno;
+    }if(ID.checked == true){
+        num[4] = retorno; 
+    }
+}
+function checkedBox1(){
     if(opt.checked == true){
         SE.checked=0;
         SD.checked=0;
         IE.checked=0;
         ID.checked=0;
     }
-}
+} 
 function checkedBox(){
+    
     if(SE.checked == true | SD.checked == true | IE.checked == true | ID.checked == true){
-        opt.checked = 0;
-        
+        opt.checked = false;
     }
 }
 
 function setRadius(){
+    
+
     /* Variaveis locais */
     const valor = document.getElementById("text").value;
     
+
     if(opt.checked == true){
+        guardaValor(valor);
         tela.style.borderRadius = valor + "px";
     }if(SE.checked == true){
+        guardaValor(valor);
         tela.style.borderTopLeftRadius = valor + "px";        
     }if(SD.checked == true){
+        guardaValor(valor);
         tela.style.borderTopRightRadius = valor + "px";
     }if(IE.checked == true){
+        guardaValor(valor);
         tela.style.borderBottomLeftRadius = valor + "px";
     }if(ID.checked == true){
+        guardaValor(valor);
         tela.style.borderBottomRightRadius = valor + "px";
     }
 }
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("text");
+function myButton() {
+    /*
+    // Get the text field 
+    //var copyText = document.getElementById("text");
   
-    /* Seleciona o campo de texto */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    // Seleciona o campo de texto 
+    //copyText.select();
+    //copyText.setSelectionRange(0, 99999); /* For mobile devices 
   
-    /* Copiando texto */
-    document.execCommand("copy");
+    // Copiando texto 
+    //document.execCommand("copy");
   
-    /* colocando codigo na tela */
-    tela.textContent = "border-radius: " + copyText.value+"px;";
+    // colocando codigo na tela 
+    //document.querySelector().innerHTML = "border-radius: " + copyText.value+"px;";*/
+    if(opt.checked == true){
+        var t = getValor(0);
+        document.querySelector("#te").innerHTML = "border-radius: "+t+ ";";  
+    }if(SE.checked == true){
+        var se = getValor(1);
+        document.querySelector('#se').innerHTML = "border-top-left-radius: " +se+ ";";            
+    }if(SD.checked == true){
+        var sd = getValor(2);
+        document.querySelector("#sd").innerHTML = "border-top-right-radius: "+sd+ ";";
+    }if(IE.checked == true){
+        var ie = getValor(3);
+        document.querySelector("#ie").innerHTML = "border-bottom-left-radius: ;"+ie+ ";";
+    }if(ID.checked == true){
+        var id = getValor(4);
+        document.querySelector("#id").innerHTML = "border-bottom-right-radius: "+id+ ";";
+    }
+
   }
