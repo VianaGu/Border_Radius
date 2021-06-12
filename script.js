49d7ac0be72af667a8566b4ad1c0086291ecd212
gustavolive0603 <gustaolive1234@gmail.com>
2021-06-11 23:18:43 -0300

    Melhora a function setRadius
    agora a function pode escolher qual canto sofre as mudanças, e a página
    já pede o foco para o campo de texto

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
+/* variaveis Globais */
 const text = document.getElementById("text");
 const button = document.getElementById("button");
+const v1=0;
+const tela = document.getElementById("mid");
+const opt = document.getElementById("test");
+const SE = document.getElementById("SuperiorEsquerdo");
+const SD = document.getElementById("SuperiorDireito");
+const IE = document.getElementById("InferiorEsquerdo");
+const ID = document.getElementById("InferiorDireito");
 
+text.focus();
+
+/* Eventos */
 text.addEventListener('keyup',setRadius);
-button.addEventListener("click", myFunction)
-
+button.addEventListener("click", myFunction);
+opt.addEventListener("click",zero);
+window.addEventListener("click", checkedBox);
+window.addEventListener("click", setRadius);
 /* Funções */
+function zero(){
+    if(opt.checked == true){
+        SE.checked=0;
+        SD.checked=0;
+        IE.checked=0;
+        ID.checked=0;
+    }
+}
+function checkedBox(){
+    if(SE.checked == true | SD.checked == true | IE.checked == true | ID.checked == true){
+        opt.checked = 0;
+        
+    }
+}
+
 function setRadius(){
     /* Variaveis locais */
     const valor = document.getElementById("text").value;
-    document.getElementById("mid").style.borderRadius = valor + "px";
+    
+    if(opt.checked == true){
+        tela.style.borderRadius = valor + "px";
+    }if(SE.checked == true){
+        tela.style.borderTopLeftRadius = valor + "px";        
+    }if(SD.checked == true){
+        tela.style.borderTopRightRadius = valor + "px";
+    }if(IE.checked == true){
+        tela.style.borderBottomLeftRadius = valor + "px";
+    }if(ID.checked == true){
+        tela.style.borderBottomRightRadius = valor + "px";
+    }
 }
 function myFunction() {
     /* Get the text field */
     var copyText = document.getElementById("text");
   
-    /* Select the text field */
+    /* Seleciona o campo de texto */
     copyText.select();
     copyText.setSelectionRange(0, 99999); /* For mobile devices */
   
-    /* Copy the text inside the text field */
+    /* Copiando texto */
     document.execCommand("copy");
   
-    /* Alert the copied text */
-    alert("border-radius: " + copyText.value+"px;");
+    /* colocando codigo na tela */
+    tela.textContent = "border-radius: " + copyText.value+"px;";
   }
